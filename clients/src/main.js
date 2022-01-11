317767c879bcd58adb20785014951b596f2065a8
LeoChien-SC <leochien@softchef.com>
2022-01-11 14:53:38 +0800

    [feat] amplify init

--- clients/src/main.js
+++ clients/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import './plugins/dayjs'
 import i18n from './plugins/i18n'
+import amplify from './plugins/amplify'
 
 Vue.config.productionTip = false
 
@@ -12,5 +13,6 @@ new Vue({
   router,
   store,
   i18n,
+  amplify,
   render: h => h(App)
 }).$mount('#app')
---
+++ clients/src/plugins/amplify.js
@@ -0,0 +1,17 @@
+import Amplify from '@aws-amplify/core'
+
+export default {
+  setAmplify(config) {
+    const awsConfig = {
+      API: {
+        endpoints: [
+          {
+            name: 'API',
+            endpoint: process.env.VUE_APP_API
+          }
+        ]
+      }
+    }
+    Amplify.configure(awsConfig)
+  }
+}
