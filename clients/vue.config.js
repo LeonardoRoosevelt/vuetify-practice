e9eb0a667c67c6386db20171a6961d13ced6dcda
LeoChien-SC <leochien@softchef.com>
2022-01-11 14:30:22 +0800

    [feat] i18n init

--- clients/vue.config.js
+++ clients/vue.config.js
@@ -5,5 +5,16 @@ module.exports = {
     } else {
       // mutate for development...
     }
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      includeLocales: false,
+      enableBridge: true
+    }
   }
 }
