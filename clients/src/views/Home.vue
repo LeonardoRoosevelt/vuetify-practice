6d67e51dee85a6ff735bf3f9fb7b97a0e35d8c5b
LeoChien-SC <leochien@softchef.com>
2022-01-11 14:34:11 +0800

    [feat] lodash init

--- clients/src/views/Home.vue
+++ clients/src/views/Home.vue
@@ -8,11 +8,21 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import _ from 'lodash'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  created () {
+    this.lodashTest()
+  },
+  methods: {
+    lodashTest () {
+      const result = _.compact([0, 1, false, 2, '', 3])
+      console.log(result)
+    }
   }
 }
 </script>
